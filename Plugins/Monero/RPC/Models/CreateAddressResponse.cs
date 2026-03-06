baa1abd6760c1efebacfe04aa295b2ff20509b24
Deverick <5827364+deverickapollo@users.noreply.github.com>
2026-03-05 22:03:02 -0600

    Add RPC models for wallet

--- Plugins/Monero/RPC/Models/CreateAddressResponse.cs
+++ Plugins/Monero/RPC/Models/CreateAddressResponse.cs
@@ -6,5 +6,7 @@ namespace BTCPayServer.Plugins.Monero.RPC.Models
     {
         [JsonProperty("address")] public string Address { get; set; }
         [JsonProperty("address_index")] public long AddressIndex { get; set; }
+        [JsonProperty("address_indices")] public long[] AddressIndices { get; set; }
+        [JsonProperty("addresses")] public string[] Addresses { get; set; }
     }
 }
--- Plugins/Monero/RPC/Models/GetAddressRequest.cs
+++ Plugins/Monero/RPC/Models/GetAddressRequest.cs
@@ -1,13 +1,23 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 
 namespace BTCPayServer.Plugins.Monero.RPC.Models;
 
 public class GetAddressRequest
 {
     [JsonProperty("account_index")] public int AccountIndex { get; set; }
+    [JsonProperty("address_index")] public int[] AddressIndex { get; set; }
 }
 
 public class GetAddressResponse
 {
     [JsonProperty("address")] public string Address { get; set; }
+    [JsonProperty("addresses")] public AddressInfo[] Addresses { get; set; }
+}
+
+public class AddressInfo
+{
+    [JsonProperty("address")] public string Address { get; set; }
+    [JsonProperty("address_index")] public long AddressIndex { get; set; }
+    [JsonProperty("label")] public string Label { get; set; }
+    [JsonProperty("used")] public bool Used { get; set; }
 }
